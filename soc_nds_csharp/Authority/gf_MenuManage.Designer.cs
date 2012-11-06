45926f4de6d0a7fcb7e7522c75ff185a8ba056b6
xcc <changchengxue@gmail.com>
2012-11-06 21:14:05 +0800

    update 2012/11/6 Ultimate

---
+++ soc_nds_csharp/Authority/gf_MenuManage.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace soc_nds_csharp.Authority
+{
+    partial class gf_MenuManage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gf_MenuManage));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsb_Add = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsb_Delete = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsb_Exit = new System.Windows.Forms.ToolStripButton();
+            this.dgv_Menu = new System.Windows.Forms.DataGridView();
+            this.panel_addMenu = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.cbo_Menu = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_subMenu = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Menu)).BeginInit();
+            this.panel_addMenu.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsb_Add,
+            this.toolStripSeparator1,
+            this.tsb_Delete,
+            this.toolStripSeparator2,
+            this.tsb_Exit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(755, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsb_Add
+            // 
+            this.tsb_Add.Image = ((System.Drawing.Image)(resources.GetObject("tsb_Add.Image")));
+            this.tsb_Add.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsb_Add.Name = "tsb_Add";
+            this.tsb_Add.Size = new System.Drawing.Size(52, 22);
+            this.tsb_Add.Text = "添加";
+            this.tsb_Add.Click += new System.EventHandler(this.tsb_Add_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsb_Delete
+            // 
+            this.tsb_Delete.Image = ((System.Drawing.Image)(resources.GetObject("tsb_Delete.Image")));
+            this.tsb_Delete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsb_Delete.Name = "tsb_Delete";
+            this.tsb_Delete.Size = new System.Drawing.Size(52, 22);
+            this.tsb_Delete.Text = "删除";
+            this.tsb_Delete.Click += new System.EventHandler(this.tsb_Delete_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tsb_Exit
+            // 
+            this.tsb_Exit.Image = ((System.Drawing.Image)(resources.GetObject("tsb_Exit.Image")));
+            this.tsb_Exit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsb_Exit.Name = "tsb_Exit";
+            this.tsb_Exit.Size = new System.Drawing.Size(52, 22);
+            this.tsb_Exit.Text = "退出";
+            this.tsb_Exit.Click += new System.EventHandler(this.tsb_Exit_Click);
+            // 
+            // dgv_Menu
+            // 
+            this.dgv_Menu.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Menu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Menu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_Menu.Location = new System.Drawing.Point(0, 25);
+            this.dgv_Menu.Name = "dgv_Menu";
+            this.dgv_Menu.ReadOnly = true;
+            this.dgv_Menu.RowTemplate.Height = 23;
+            this.dgv_Menu.Size = new System.Drawing.Size(755, 397);
+            this.dgv_Menu.TabIndex = 1;
+            // 
+            // panel_addMenu
+            // 
+            this.panel_addMenu.Controls.Add(this.groupBox1);
+            this.panel_addMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_addMenu.Location = new System.Drawing.Point(0, 25);
+            this.panel_addMenu.Name = "panel_addMenu";
+            this.panel_addMenu.Size = new System.Drawing.Size(755, 81);
+            this.panel_addMenu.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.groupBox1.Controls.Add(this.txt_subMenu);
+            this.groupBox1.Controls.Add(this.btn_Open);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btn_Add);
+            this.groupBox1.Controls.Add(this.btn_Cancel);
+            this.groupBox1.Controls.Add(this.cbo_Menu);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(730, 70);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "菜单添加";
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(467, 16);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(75, 23);
+            this.btn_Open.TabIndex = 6;
+            this.btn_Open.Text = " 打 开 ";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "选择子菜单：";
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Add.Location = new System.Drawing.Point(547, 40);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
+            this.btn_Add.TabIndex = 4;
+            this.btn_Add.Text = " 提 交 ";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Cancel.Location = new System.Drawing.Point(637, 40);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 3;
+            this.btn_Cancel.Text = " 取 消 ";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // cbo_Menu
+            // 
+            this.cbo_Menu.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbo_Menu.FormattingEnabled = true;
+            this.cbo_Menu.Location = new System.Drawing.Point(62, 19);
+            this.cbo_Menu.Name = "cbo_Menu";
+            this.cbo_Menu.Size = new System.Drawing.Size(121, 20);
+            this.cbo_Menu.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "主菜单:";
+            // 
+            // txt_subMenu
+            // 
+            this.txt_subMenu.Location = new System.Drawing.Point(275, 11);
+            this.txt_subMenu.Name = "txt_subMenu";
+            this.txt_subMenu.Size = new System.Drawing.Size(173, 47);
+            this.txt_subMenu.TabIndex = 7;
+            this.txt_subMenu.Text = "";
+            // 
+            // gf_MenuManage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 422);
+            this.Controls.Add(this.panel_addMenu);
+            this.Controls.Add(this.dgv_Menu);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "gf_MenuManage";
+            this.Text = "菜单管理";
+            this.Load += new System.EventHandler(this.gf_MenuManage_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Menu)).EndInit();
+            this.panel_addMenu.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsb_Add;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tsb_Delete;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton tsb_Exit;
+        private System.Windows.Forms.DataGridView dgv_Menu;
+        private System.Windows.Forms.Panel panel_addMenu;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.ComboBox cbo_Menu;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txt_subMenu;
+    }
+}
---
+++ soc_nds_csharp/Authority/gf_MenuManage.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using HDICSoft.Message;
+using HDICSoft.DB;
+using HDICSoft.Command;
+
+namespace soc_nds_csharp.Authority
+{
+    public partial class gf_MenuManage : Form
+    {
+        string path = Application.StartupPath;
+           
+        public gf_MenuManage()
+        {
+            InitializeComponent();
+        }
+
+        private void gf_MenuManage_Load(object sender, EventArgs e)
+        {
+            init();
+            bindDgv();
+            txt_subMenu.BackColor = HDIC_Command.setColor();
+            dgv_Menu.BackgroundColor = HDIC_Command.setColor();
+            panel_addMenu.BackColor = HDIC_Command.setColor();
+            groupBox1.BackColor = HDIC_Command.setColor();
+
+            path = path.Substring(0,path.Substring(0, path.LastIndexOf("\\")).LastIndexOf("\\"));
+
+        }
+
+        private void init()
+        {
+            panel_addMenu.Visible = false;
+            txt_subMenu.Text = "";
+
+            cbo_Menu.DataSource = HDIC_DB.GetList("select distinct menuNo,menuName from SysMenuDisplay where isModel=1");
+            cbo_Menu.DisplayMember = "menuName";
+            cbo_Menu.ValueMember = "menuNo";
+            cbo_Menu.SelectedIndex = -1;
+        }
+
+        private void bindDgv()
+        {
+            dgv_Menu.DataSource = HDIC_DB.GetList("select * from SysMenuDisplay order by menuNo");
+        }
+
+        private bool checkPara()
+        {
+            if (txt_subMenu.Text.Trim() == "")
+            {
+                HDIC_Message.ShowWarnDialog(this, "没有选择子菜单");
+                return false;
+            }
+            if (cbo_Menu.SelectedIndex==-1)
+            {
+                HDIC_Message.ShowWarnDialog(this, "没有选择主菜单");
+                return false;
+            }
+            return true;
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog1 =new OpenFileDialog();
+            OpenFileDialog1.InitialDirectory=path;
+            OpenFileDialog1.Filter = "(c#程序)*.cs|*.cs";
+            if (OpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string addr = OpenFileDialog1.FileName;
+                    addr = addr.Substring(addr.LastIndexOf("soc_nds_csharp"));
+                    txt_subMenu.Text = addr.Substring(0,addr.LastIndexOf("."));
+                    txt_subMenu.Text = txt_subMenu.Text.Replace("\\", ".");
+                }
+                catch (System.Exception ex)
+                {
+                    HDIC_Message.ShowWarnDialog(this, ex.ToString());
+                }
+            }
+        }
+
+        #region Panel
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            if (!checkPara())
+            {
+                return;
+            }
+            string sqlstr = "select max(menuNo) from SysMenuDisplay where menuNo like '" + cbo_Menu.SelectedValue.ToString().Trim() + "%'";
+            string ID = (Convert.ToInt32(HDIC_DB.sqlQuery(sqlstr))+1).ToString().Trim();
+            sqlstr = @"insert into SysMenuDisplay values('0" + ID + "','" + this.Text.Trim() + "','" + txt_subMenu.Text.Trim() + "',null,'" +
+                    cbo_Menu.SelectedValue.ToString().Trim()+"',0)";
+            if (HDIC_DB.ExcuteNonQuery(sqlstr, null) > 0)
+            {
+                HDIC_Message.ShowInfoDialog(this, "添加成功");
+                init();
+                bindDgv(); 
+            }
+
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+        #endregion
+
+        #region Menu
+        private void tsb_Add_Click(object sender, EventArgs e)
+        {
+            init();
+            panel_addMenu.Visible = true;
+        }
+
+        private void tsb_Delete_Click(object sender, EventArgs e)
+        {
+            init();
+            if (dgv_Menu.Rows.Count > 0)
+            {
+                if (dgv_Menu.CurrentRow.Index > -1)
+                {
+                    if (HDIC_DB.sqlQuery("select count(menuNo) from dbo.SysMenuDisplay where isModel=1 and menuNo='" + dgv_Menu.CurrentRow.Cells["menuNo"].Value.ToString().Trim() + "'") != "0")
+                    {
+                        HDIC_Message.ShowWarnDialog(this, "不能删除主菜单");
+                    }
+                    else
+                    {
+                        if (HDIC_Message.ShowQuestionDialog(this, "确定要删除：《" + dgv_Menu.CurrentRow.Cells["menuName"].Value.ToString().Trim() + "》吗？") == DialogResult.OK)
+                        {
+                            if (HDIC_DB.ExcuteNonQuery("delete SysMenuDisplay where menuNo='" + dgv_Menu.CurrentRow.Cells["menuNo"].Value.ToString().Trim() + "'", null) > 0)
+                            {
+                                HDIC_Message.ShowInfoDialog(this, "删除成功");
+                                bindDgv();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void tsb_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
