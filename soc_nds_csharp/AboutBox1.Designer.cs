4405ed336d966f882e194e8e3b2fc0c8e4564044
xcc <cc.xue@hdigroup.cn>
2013-01-17 18:24:22 +0800

    update

---
+++ soc_nds_csharp/AboutBox1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace soc_nds_csharp
+{
+    partial class AboutBox1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox1));
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.lbl_Author = new System.Windows.Forms.Label();
+            this.labelCopyright = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelCompanyName = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lbl_CLR = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lbl_Memory = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_CurrentUser = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.lbl_CurrentOS = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.richText_Description = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
+            this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(131, 297);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoPictureBox.TabIndex = 13;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(171, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "产品名称：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(171, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "版本：";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.Location = new System.Drawing.Point(248, 16);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(194, 16);
+            this.labelProductName.TabIndex = 17;
+            this.labelProductName.Text = "版本名称：";
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.Location = new System.Drawing.Point(248, 35);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(194, 16);
+            this.labelVersion.TabIndex = 17;
+            this.labelVersion.Text = "版本名称：";
+            // 
+            // lbl_Author
+            // 
+            this.lbl_Author.Location = new System.Drawing.Point(248, 75);
+            this.lbl_Author.Name = "lbl_Author";
+            this.lbl_Author.Size = new System.Drawing.Size(194, 16);
+            this.lbl_Author.TabIndex = 20;
+            this.lbl_Author.Text = "版本名称：";
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.Location = new System.Drawing.Point(248, 56);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(194, 16);
+            this.labelCopyright.TabIndex = 21;
+            this.labelCopyright.Text = "版本名称：";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(171, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 16);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "作者：";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(171, 54);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 16);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "版权：";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelCompanyName
+            // 
+            this.labelCompanyName.Location = new System.Drawing.Point(248, 94);
+            this.labelCompanyName.Name = "labelCompanyName";
+            this.labelCompanyName.Size = new System.Drawing.Size(194, 16);
+            this.labelCompanyName.TabIndex = 23;
+            this.labelCompanyName.Text = "。";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(171, 92);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 16);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "公司名称：";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_CLR
+            // 
+            this.lbl_CLR.Location = new System.Drawing.Point(248, 112);
+            this.lbl_CLR.Name = "lbl_CLR";
+            this.lbl_CLR.Size = new System.Drawing.Size(194, 16);
+            this.lbl_CLR.TabIndex = 25;
+            this.lbl_CLR.Text = "。";
+            // 
+            // label13
+            // 
+            this.label13.Location = new System.Drawing.Point(140, 110);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(102, 16);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "公共语言运行库：";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_Memory
+            // 
+            this.lbl_Memory.Location = new System.Drawing.Point(248, 129);
+            this.lbl_Memory.Name = "lbl_Memory";
+            this.lbl_Memory.Size = new System.Drawing.Size(194, 16);
+            this.lbl_Memory.TabIndex = 27;
+            this.lbl_Memory.Text = "。";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(140, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 16);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "程序内存使用：";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_CurrentUser
+            // 
+            this.lbl_CurrentUser.Location = new System.Drawing.Point(248, 145);
+            this.lbl_CurrentUser.Name = "lbl_CurrentUser";
+            this.lbl_CurrentUser.Size = new System.Drawing.Size(194, 16);
+            this.lbl_CurrentUser.TabIndex = 29;
+            this.lbl_CurrentUser.Text = "。";
+            // 
+            // label16
+            // 
+            this.label16.Location = new System.Drawing.Point(140, 143);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(102, 16);
+            this.label16.TabIndex = 28;
+            this.label16.Text = "当前使用用户：";
+            this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_CurrentOS
+            // 
+            this.lbl_CurrentOS.Location = new System.Drawing.Point(248, 161);
+            this.lbl_CurrentOS.Name = "lbl_CurrentOS";
+            this.lbl_CurrentOS.Size = new System.Drawing.Size(194, 16);
+            this.lbl_CurrentOS.TabIndex = 31;
+            this.lbl_CurrentOS.Text = "。";
+            // 
+            // label18
+            // 
+            this.label18.Location = new System.Drawing.Point(140, 159);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(102, 16);
+            this.label18.TabIndex = 30;
+            this.label18.Text = "当前操作系统：";
+            this.label18.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // richText_Description
+            // 
+            this.richText_Description.Enabled = false;
+            this.richText_Description.Location = new System.Drawing.Point(157, 217);
+            this.richText_Description.Name = "richText_Description";
+            this.richText_Description.Size = new System.Drawing.Size(285, 79);
+            this.richText_Description.TabIndex = 32;
+            this.richText_Description.Text = "             有问题请联系作者本人\n           EMAIL:cc.xue@hdigroup.cn\n                QQ:76" +
+                "5650886";
+            // 
+            // AboutBox1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 302);
+            this.Controls.Add(this.richText_Description);
+            this.Controls.Add(this.lbl_CurrentOS);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.lbl_CurrentUser);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.lbl_Memory);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_CLR);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.labelCompanyName);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lbl_Author);
+            this.Controls.Add(this.labelCopyright);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.labelProductName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logoPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutBox1";
+            this.Padding = new System.Windows.Forms.Padding(9, 8, 9, 8);
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "说明";
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelProductName;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label lbl_Author;
+        private System.Windows.Forms.Label labelCopyright;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelCompanyName;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lbl_CLR;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lbl_Memory;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbl_CurrentUser;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label lbl_CurrentOS;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.RichTextBox richText_Description;
+
+
+    }
+}
---
+++ soc_nds_csharp/AboutBox1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace soc_nds_csharp
+{
+    partial class AboutBox1 : Form
+    {
+        public AboutBox1()
+        {
+            InitializeComponent();
+            this.Text = String.Format("关于 {0}", AssemblyTitle);
+            this.labelProductName.Text = AssemblyProduct;
+            this.labelVersion.Text = String.Format("版本 {0}", AssemblyVersion);
+            this.labelCopyright.Text = AssemblyCopyright;
+            lbl_Author.Text ="薛长城";
+            this.labelCompanyName.Text = AssemblyCompany;
+            lbl_CLR.Text = Environment.Version.ToString();
+            lbl_Memory.Text = Environment.WorkingSet.ToString("N0") + " bytes";
+            lbl_CurrentUser.Text = Environment.UserName + " @ " + Environment.UserDomainName + "\\" + Environment.UserDomainName != Environment.MachineName? Environment.MachineName:"";
+            lbl_CurrentOS.Text = Environment.OSVersion.ToString();
+            //this.richText_Description.Text = AssemblyDescription;
+            //richText_Description.Enabled = false;
+            //richText_Description.SelectionAlignment = HorizontalAlignment.Center;
+           
+        }
+
+        #region 程序集特性访问器
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+
+        #endregion
+    }
+}
